9fc80cd73c63ed76edf85f4390d7b542b385003a
lucasolasz <olaszlucas072@gmail.com>
2021-12-10 00:43:59 -0300

    Corrige mensagem de erro de Jquery

--- header.php
+++ header.php
@@ -11,7 +11,18 @@
 <script type="text/javascript" src="/bootstrap/js/jquery.maskedinput-1.1.4.pack.js"></script>
 <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script> 
-
+<script>
+ //Remove erro do Jquery
+ jQuery.browser = {};
+        (function () {
+            jQuery.browser.msie = false;
+            jQuery.browser.version = 0;
+            if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+                jQuery.browser.msie = true;
+                jQuery.browser.version = RegExp.$1;
+            }
+        })();
+</script>
 
 
 <title>SF SYSTEM</title>
