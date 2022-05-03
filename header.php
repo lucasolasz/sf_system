443658a44932fd9f51138fa41e2a445e5c2aa626
lucasolasz <olaszlucas072@gmail.com>
2022-05-03 16:01:20 -0300

    Adiciona o dataTables ao projeto

--- header.php
+++ header.php
@@ -14,13 +14,15 @@
 <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- <link rel="stylesheet" href="/resources/demos/style.css"> -->
 <!-- <link rel="stylesheet" href="/bootstrap-datepicker/bootstrap-datepicker.css"> -->
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+<!-- <link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" /> -->
 <link rel="shortcut icon" href="/img/favicon.ico">
+<link rel="stylesheet" href="/dataTables/datatables.min.css">
 
 <script src="/bootstrap/js/jquery-3.6.0.min.js"></script>
 <script src="/bootstrap/js/jquery.maskedinput-1.1.4.pack.js"></script>
 <script src="/bootstrap/js/bootstrap.min.js"></script>
-<script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+<script src="/dataTables/datatables.min.js"></script>
+<!-- <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script> -->
 
 
 <script>
